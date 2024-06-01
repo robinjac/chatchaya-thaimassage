b02eb79ab91e38bd10aee8cd910fec79e821ea75
robin jacobsson <robin23jac@gmail.com>
2024-06-01 17:15:09 +0200

    Make some fixes to make tailwind work

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <>
-      <p>
+      <p className="text-xl text-bold">
         Hello and welcome to Chatchaya Thaimassage
       </p>
     </>
