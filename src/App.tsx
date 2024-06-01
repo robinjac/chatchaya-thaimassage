7f45f71e9f9129131215fca9624669fe7fb7eb89
robin jacobsson <robin23jac@gmail.com>
2024-06-01 17:04:08 +0200

    Remove some stuff and change to tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <p>
+        Hello and welcome to Chatchaya Thaimassage
+      </p>
+    </>
+  );
+}
+
+export default App;
