92b0607318ca05c483d25958fedadaecca82e06c
robin jacobsson <robin23jac@gmail.com>
2024-06-02 11:46:25 +0200

    Set therapies section to full screen

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,22 @@ const App = () => {
       <AuroraBackground>
         <Banner />
       </AuroraBackground>
-      {therapies.map(({ category, services }, index) => (
-        <section
-          key={category + index}
-          className="relative container flex flex-col items-center w-full space-y-8 p-6"
-        >
-          <h2 className="text-3xl font-bold">{category}</h2>
-          <div className="grid w-fit gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-            {services.map((therapy) => (
-              <TherapyCard key={index} {...therapy} />
-            ))}
-          </div>
-        </section>
-      ))}
-
-      <section className="relative container flex flex-col items-center  w-full space-y-8 p-6">
+      <div className="relative w-screen h-screen">
+        {therapies.map(({ category, services }, index) => (
+          <section
+            key={category + index}
+            className="relative flex flex-col items-center w-screen space-y-8 p-6"
+          >
+            <h2 className="text-3xl font-bold">{category}</h2>
+            <div className="grid w-fit gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+              {services.map((therapy) => (
+                <TherapyCard key={index} {...therapy} />
+              ))}
+            </div>
+          </section>
+        ))}
+      </div>
+      <section className="relative flex flex-col items-center w-screen space-y-8 p-6">
         <h2 className="text-3xl font-bold text-center">Reviews</h2>
         <InfiniteMovingCards items={reviews} direction="right" speed="slow" />
       </section>
