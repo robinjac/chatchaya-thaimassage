bc81c7d1d5c77c3e43e87868e0e350042d968876
robin jacobsson <robin23jac@gmail.com>
2024-06-02 12:27:58 +0200

    Link to location and boka direkt

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,12 +5,12 @@ const Banner = () => {
     <div className="relative flex justify-center items-center flex-col gap-6 w-screen h-screen max-h-[400px]">
       <h1 className="text-5xl w-full text-center px-12">Chatchaya Thaimassage</h1>
       <div className="flex gap-2">
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+        <a href="https://g.co/kgs/GWU8n3E" className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
           Hitta hit
-        </button>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+        </a>
+        <a href="https://www.bokadirekt.se/places/chat-cha-ya-thaimassage-58430" className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
           Boka direkt
-        </button>
+        </a>
       </div>
     </div>
   );
