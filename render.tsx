cbe8e55e9a55a2a0442f3682c16918895d5803b6
robin jacobsson <robin23jac@gmail.com>
2024-06-18 14:56:51 +0200

    Fix css url in html output

--- render.tsx
+++ render.tsx
@@ -5,10 +5,15 @@ import App from "./src/App";
 const html = renderToStaticMarkup(<App />);
 const index = await file("index.html").text();
 
-const result = index.replace(
-  `<div class="absolute inset-0 overflow-x-hidden" id="root"></div>`,
-  `<div class="absolute inset-0 overflow-x-hidden" id="root">${html}</div>`
-);
+const result = index
+  .replace(
+    `<div class="absolute inset-0 overflow-x-hidden" id="root"></div>`,
+    `<div class="absolute inset-0 overflow-x-hidden" id="root">${html}</div>`
+  )
+  .replace(
+    '<link rel="stylesheet" href="/styles.css" />',
+    '<link rel="stylesheet" href="./styles.css" />'
+  );
 
 await write("./dist/index.html", result);
 
