6fb26b6a32a8471fe9dc7aed013ba4c42407ef9d
robin jacobsson <robin23jac@gmail.com>
2024-06-22 00:00:09 +0200

    Move scripts inside render

--- render.tsx
+++ render.tsx
@@ -1,28 +1,25 @@
-import { write, file } from "bun";
-import { renderToStaticMarkup } from "react-dom/server";
+import { write, file, $ } from "bun";
+import { renderToString } from "react-dom/server";
 import App from "./src/App";
 
+import { rmdir } from "node:fs/promises";
 
-import { readdir, rmdir } from "node:fs/promises";
+const stylesID = crypto.randomUUID();
+const productionID = crypto.randomUUID();
 
+console.log("Clering old files");
 
-/*
-// read all the files in the current directory, recursively
-const files = await readdir("./src/assets/images", { recursive: true });
+await rmdir("./dist", { recursive: true });
 
-for (const fileName of files) {
-  const text = await file("./dist/" + fileName).text();
+console.log("\n Building CSS");
 
-  await write("./dist/" + fileName, text);
-}
- */
+await $`NODE_ENV=production bunx tailwindcss -m -i ./styles.css -o ./dist/styles.${stylesID}.css`;
 
+console.log("\n Building JS");
 
-await rmdir("./dist", { recursive: true })
+await $`NODE_ENV=production bun build --minify ./src/production.tsx --outdir ./dist --entry-naming production.${productionID}.js "`;
 
-console.log("clering old dist")
-
-const html = renderToStaticMarkup(<App />);
+const html = renderToString(<App />);
 
 const index = await file("index.html").text();
 
@@ -33,13 +30,13 @@ const result = index
   )
   .replace(
     '<link rel="stylesheet" href="/styles.css" />',
-    '<link rel="stylesheet" href="./styles.css" />'
+    `<link rel="stylesheet" href="./styles.${stylesID}.css" />`
   )
   .replace(
     '<script type="module" src="/src/development.tsx"></script>',
-    '<script type="module" src="./production.js"></script>'
+    `<script type="module" src="./production.${productionID}.js"></script>`
   );
 
-await write(`./dist/index.${crypto.randomUUID()}.html`, result);
+await write(`./dist/index.html`, result);
 
 console.log("done!");
